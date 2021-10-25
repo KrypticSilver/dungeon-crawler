ac90aed45566ec2b5a7ff81b023f728b58c3046f
Toby Hogan <tobyhogan44@gmail.com>
2021-10-25 12:15:49 +0100

    Worked on ball implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Paddle from "/src/paddle.js";
 import InputHandler from "/src/input.js";
+import Ball from "/src/ball.js";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -10,20 +11,22 @@ const GAME_HEIGHT = 600;
 ctx.clearRect(0, 0, 800, 600);
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let ball = new Ball();
 
 new InputHandler(paddle);
 
 let lastTime = 0;
 
+
 function gameLoop(timestamp) {
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
 
-
-    ctx.clearRect(0, 0, 800, 600);
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     paddle.update(deltaTime);
     paddle.draw(ctx);
 
+    ball.draw(ctx);
 
     requestAnimationFrame(gameLoop);
 }
